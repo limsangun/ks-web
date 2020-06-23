2ad42d4dd590007b174f05b38fbd46d4a1c40a64
limsang <tkrnfldlekd@gmail.com>
2020-06-23 22:36:28 +0900

    init

---
+++ src/main/java/kr/ac/ks/app/domain/Lesson.java
@@ -0,0 +1,35 @@
+package kr.ac.ks.app.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Lesson {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private int quota;
+    private int regCnt; //수강신청 한사람
+
+    @OneToMany
+    private List<Course> courses = new ArrayList<>();
+
+    public Lesson() {
+    }
+
+    @Builder
+    public Lesson(String name, int quota) {
+        this.name = name;
+        this.quota = quota; // 총 수강 가능한 인원
+        this.regCnt = regCnt; // 수강신청한 인원
+    }
+}
